4a3783ba91c5217e306f2f62ad49f70448740696
Edwin Fandiño <edwineladio73@gmail.com>
2024-02-20 08:57:58 -0500

    Create project

---
+++ build/web/index.jsp
@@ -0,0 +1,22 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>pirobo</title>
+    </head>
+    <body>
+        <h1>formulario asqueroso</h1>
+        
+        <form method="post" action="Registro">
+            <label>Usuario: </label><br>
+            <input type="email" name="txtUser" placeholder="Usuario">
+            
+            <label>Contraseña:</label><br>
+            <input type="password" name="txtPass" placeholder="Contraseña">
+            
+            <input type="submit" name="btnEnviar" value="Enviar">
+        </form>
+    </body>
+</html>
---
+++ src/java/Modelo/Usuario.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Usuario {
+    
+    private String usuario;
+    private String pass;
+
+    public Usuario(String usuario, String pass) {
+        this.usuario = usuario;
+        this.pass = pass;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "usuario=" + usuario + ", pass=" + pass + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        if (!Objects.equals(this.usuario, other.usuario)) {
+            return false;
+        }
+        return Objects.equals(this.pass, other.pass);
+    }
+    
+    
+    
+}
